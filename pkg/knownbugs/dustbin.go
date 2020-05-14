d28573ac0c4e479523047d5aa2fed0c87a266a68
Illyrix <Illyrix@outlook.com>
2020-05-14 13:19:00 +0800

    reconstruct known bug functions
    Signed-off-by: Illyrix <Illyrix@outlook.com>

--- pkg/knownbugs/dustbin.go
+++ pkg/knownbugs/dustbin.go
@@ -17,9 +17,24 @@ type Dustbin struct {
 	// may add some configs and contexts
 }
 type KnownBug = func(*Dustbin) bool
+type NodeCb = func(ast.Node) (ast.Node, bool)
+type Visitor struct {
+	enterFunc NodeCb
+	leaveFunc NodeCb
+}
+
+func NewVisitor() Visitor {
+	return Visitor{
+		enterFunc: emptyNodeCb,
+		leaveFunc: emptyNodeCb,
+	}
+}
 
 var (
-	Bugs map[string]KnownBug = make(map[string]KnownBug)
+	Bugs        map[string]KnownBug = make(map[string]KnownBug)
+	emptyNodeCb NodeCb              = func(in ast.Node) (ast.Node, bool) {
+		return in, true
+	}
 )
 
 func NewDustbin(stmts []ast.Node, pivots map[string]*connection.QueryItem) Dustbin {
@@ -41,4 +56,28 @@ func (d *Dustbin) IsKnownBug() bool {
 
 func init() {
 	Bugs["issue16788"] = issue16788
+}
+
+func (v *Visitor) Enter(in ast.Node) (out ast.Node, skipChildren bool) {
+	return v.enterFunc(in)
+}
+
+func (v *Visitor) Leave(in ast.Node) (out ast.Node, ok bool) {
+	return v.leaveFunc(in)
+}
+
+func (v *Visitor) SetEnter(e NodeCb) {
+	v.enterFunc = e
+}
+
+func (v *Visitor) SetLeave(e NodeCb) {
+	v.leaveFunc = e
+}
+
+func (v *Visitor) ClearEnter() {
+	v.enterFunc = emptyNodeCb
+}
+
+func (v *Visitor) ClearLeave() {
+	v.leaveFunc = emptyNodeCb
 }
--- pkg/knownbugs/issue16788.go
+++ pkg/knownbugs/issue16788.go
@@ -1,8 +1,6 @@
 package knownbugs
 
 import (
-	"fmt"
-
 	"github.com/pingcap/parser/ast"
 	"github.com/pingcap/parser/mysql"
 	"github.com/pingcap/parser/opcode"
@@ -11,94 +9,98 @@ import (
 var (
 	issue16788 KnownBug = func(d *Dustbin) bool {
 
-		var travel func(ast.Node, bool) bool
-		travel = func(n ast.Node, found bool) bool {
-			if found {
-				return true
-			}
-			switch tp := n.(type) {
-			case *ast.ParenthesesExpr:
-				return travel(tp.Expr, false)
-			case *ast.BinaryOperationExpr:
-				if tp.Op != opcode.EQ {
-					return travel(tp.L, false) || travel(tp.R, false)
-				}
-				if lType, ok := tp.L.(ast.ValueExpr); ok {
-					if lType.GetType().Tp != mysql.TypeFloat && lType.GetType().Tp != mysql.TypeDouble {
-						return false
-					}
-					if rType, ok := tp.R.(*ast.ColumnNameExpr); !ok {
-						return false
-					} else {
-						rTp := rType.GetType().Tp
-						if rTp != mysql.TypeInt24 &&
-							rTp != mysql.TypeLong &&
-							rTp != mysql.TypeLonglong &&
-							rTp != mysql.TypeShort &&
-							rTp != mysql.TypeTiny {
-							return false
-						}
-						// TODO: select from pivot row and verify it is NULL
-
-						return true
-					}
-				}
-				if rType, ok := tp.R.(ast.ValueExpr); ok {
-					if rType.GetType().Tp != mysql.TypeFloat && rType.GetType().Tp != mysql.TypeDouble {
-						return false
-					}
-					if lType, ok := tp.L.(*ast.ColumnNameExpr); !ok {
-						return false
-					} else {
-						lTp := lType.GetType().Tp
-						if lTp != mysql.TypeInt24 &&
-							lTp != mysql.TypeLong &&
-							lTp != mysql.TypeLonglong &&
-							lTp != mysql.TypeShort &&
-							lTp != mysql.TypeTiny {
-							return false
-						}
-						// TODO: select from pivot row and verify it is NULL
-
-						return true
-					}
-				}
-				return false
-			case *ast.UnaryOperationExpr:
-				return travel(tp.V, false)
-			case *ast.IsNullExpr:
-				return travel(tp.Expr, false)
-			case *ast.ColumnNameExpr:
-				return false
-			case ast.ValueExpr:
-				return false
-			default:
-				panic(fmt.Sprintf("unreachable, tp: %+v", tp))
-			}
-		}
-
-		var mergeOnClause func(ast.Node) []ast.ExprNode
-		mergeOnClause = func(n ast.Node) []ast.ExprNode {
+		v := NewVisitor()
+		mergeOnClause := func(n ast.Node) []ast.ExprNode {
 			result := make([]ast.ExprNode, 0)
-			switch t := n.(type) {
-			case *ast.Join:
-				result = append(result, t.On.Expr)
-				result = append(result, mergeOnClause(t.Left)...)
-				result = append(result, mergeOnClause(t.Right)...)
-			}
-
+			v.SetEnter(func(in ast.Node) (ast.Node, bool) {
+				switch t := in.(type) {
+				case *ast.Join:
+					if t.On != nil {
+						result = append(result, t.On.Expr)
+					}
+				}
+				return in, false
+			})
+			defer func() {
+				v.ClearEnter()
+			}()
+			n.Accept(&v)
 			return result
 		}
 
-		fmt.Println("LENGTH: ", len(d.Stmts))
-		// get last one stmt
+		find := func(n ast.Node) bool {
+			result := false
+			v.SetEnter(func(in ast.Node) (ast.Node, bool) {
+				switch tp := in.(type) {
+				case *ast.ColumnNameExpr:
+					return in, true
+				case ast.ValueExpr:
+					return in, true
+				case *ast.BinaryOperationExpr:
+					if tp.Op != opcode.EQ {
+						return in, false
+					}
+					if lType, ok := tp.L.(ast.ValueExpr); ok {
+						if lType.GetType().Tp != mysql.TypeFloat && lType.GetType().Tp != mysql.TypeDouble && lType.GetType().Tp != mysql.TypeNewDecimal {
+							return in, false
+						}
+						if rType, ok := tp.R.(*ast.ColumnNameExpr); !ok {
+							return in, false
+						} else {
+							rTp := rType.GetType().Tp
+							if rTp != mysql.TypeInt24 &&
+								rTp != mysql.TypeLong &&
+								rTp != mysql.TypeLonglong &&
+								rTp != mysql.TypeShort &&
+								rTp != mysql.TypeTiny {
+								return in, false
+							}
+							// TODO: select from pivot row and verify it is NULL
+
+							result = true
+							return in, true
+						}
+					}
+					if rType, ok := tp.R.(ast.ValueExpr); ok {
+						if rType.GetType().Tp != mysql.TypeFloat && rType.GetType().Tp != mysql.TypeDouble && rType.GetType().Tp != mysql.TypeNewDecimal {
+							return in, false
+						}
+						if lType, ok := tp.L.(*ast.ColumnNameExpr); !ok {
+							return in, false
+						} else {
+							lTp := lType.GetType().Tp
+							if lTp != mysql.TypeInt24 &&
+								lTp != mysql.TypeLong &&
+								lTp != mysql.TypeLonglong &&
+								lTp != mysql.TypeShort &&
+								lTp != mysql.TypeTiny {
+								return in, false
+							}
+							// TODO: select from pivot row and verify it is NULL
+
+							result = true
+							return in, true
+						}
+					}
+					return in, false
+				default:
+					return in, false
+				}
+			})
+			defer func() {
+				v.ClearEnter()
+			}()
+			n.Accept(&v)
+			return result
+		}
+
 		stmt := d.Stmts[:len(d.Stmts)][0]
 		switch t := stmt.(type) {
 		case *ast.SelectStmt:
 			exprs := mergeOnClause(t.From.TableRefs)
 			exprs = append(exprs, t.Where)
 			for _, e := range exprs {
-				if travel(e, false) {
+				if find(e) {
 					return true
 				}
 			}
